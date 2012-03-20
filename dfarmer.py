b9a30027721fe5e6164cb2d7b6ecd390dbce5999
dcollins4096 <none@none>
2012-03-20 10:43:20 -0600

    added dfarmer

---
+++ dfarmer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import os, sys
+import re
+
+def scrub_output():
+
+    if 'USER' in os.environ:
+        user = os.environ['USER']
+    else:
+        print "No user in environment.  Rework script"
+        return -1
+    if 'StatFile' in os.environ:
+        sptr = open(os.environ['StatFile'])
+        slines = sptr.readlines()
+        sptr.close()
+    else:
+        print "No StatFile in environ.  Rework script"
+        return -1
+    if 'QueuedRuns' in os.environ:
+        qptr = open(os.environ['QueuedRuns'])
+        qlines = qptr.readlines()
+        qptr.close()
+    else:
+        print "No QueuedRuns in environ.  Rework script"
+        return -1
+
+    JobIDs = []
+    qhash = {}
+    """build has of jobs in queue file."""
+    debug = 0
+    if debug > 0:
+        print "==== ALL QUEUED ===="
+    for q in qlines:
+        jobid = q.split(" ")[1]
+        qhash[jobid] = q[:-1]
+        if debug > 0:
+            print jobid, qhash[jobid]
+
+
+    """this can be generalized by machine. Setup for the moab system."""
+    trigger_lines = [r'^active jobs',r'^eligible jobs', r'^blocked jobs']
+
+    trigger_reg = [re.compile(lll) for lll in trigger_lines]
+
+    for sl in slines:
+        match = None
+        sl_split = sl.split(" ")
+        if user in sl_split:
+            JobIDs.append( sl_split[0])
+        for nr, reg in enumerate(trigger_reg):
+            match = reg.match(sl)
+            if match is not None:
+                trigger_reg.pop(nr)
+                break
+        if match is not None:
+            for j in JobIDs:
+                print qhash[j]
+                qhash.pop(j)
+            JobIDs = []
+            if debug > 2:
+                print "==== Remaining QUEUED ===="
+                for q in qhash.keys():
+                    print q, qhash[q]
+
+        print sl,
+    print "finished jobs ----------"
+    for q in qhash.keys():
+        print qhash[q]
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    scrub_output()
+
+
+
+#end
