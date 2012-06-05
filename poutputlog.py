06a33581e98b08500c276f0077399b17167592b6
dcollins4096 <none@none>
2012-06-05 11:25:37 -0600

    adding alternate OutputLog name

--- poutputlog.py
+++ poutputlog.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+import sys
+OutputLog = 'OutputLog'
+if len(sys.argv) > 1:
+    OutputLog = sys.argv[1]
 
-fptr = open('OutputLog','r')
+fptr = open(OutputLog,'r')
 for n, line in enumerate(fptr):
     print n, line,
 fptr.close
