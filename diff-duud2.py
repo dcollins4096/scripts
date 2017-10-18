9ecf6b15444c7e2ccfe459ba8cc470a66ef13e01
dcollins4096 <none@none>
2017-10-17 23:17:49 -0400

    seems to work.

--- diff-duud2.py
+++ diff-duud2.py
@@ -14,13 +14,18 @@ import numpy as np
 nar = np.array
 
 parser = OptionParser()
-parser.add_option("-H","--Hierarchy",dest="hierarchy",action="store_true",default=False,
-                              help="takes list of hierarchy files")
-parser.add_option("-b","--backup",dest="backup",action="store_true",default=False,
-                              help="Backup old outputlog in OldOutputLog")
-parser.add_option("-o","--output",dest="output",action="store",default="OutputLog",
-                              help="Name of output log file")
+parser.add_option("-v","--verb",dest="verb",action="store",default=0, 
+        help="Verbosity level.  \n\t -1: diff-style report\n\t 3: debug output.")
+parser.add_option("-r","--report",dest="report",action="store",default=None,
+                              help="Report to run.  ")
+parser.add_option("-s","--slice",dest="slice",action="store",default=None,
+                              help="Only every N lines consumed from the files.")
 (options,args)=parser.parse_args()
+options.verb = int(options.verb)
+if options.slice is not None:
+    options.slice = int(options.slice)
+if options.report is not None:
+    options.report = int(options.report)
 
 def tab_is_white(string):
     tablist=string.split("\t")
@@ -66,14 +71,15 @@ class duud():
         self.removed_lines=[]
         self.date=None
         self.longest_line=-1
-    def ingest(self,ingest_slice=slice(None)):
+    def ingest(self,ingest_slice=slice(None), verb = 0):
         fptr = open(self.fname,'r')
         for line in fptr.readlines():
             self.lines.append(tab_is_white(line[:-1]))
         fptr.close()
         for line in self.lines[ingest_slice]:
             self.longest_line=max([len(line), self.longest_line])
-            print line
+            if verb == 1:
+                print line
             try:
                 line_start = line.index("/")
             except:
@@ -85,6 +91,36 @@ class duud():
             self.sizes_bytes.append(getnum(byte_chunk))
         self.delta=np.zeros(len(self.sizes_bytes))
 
+    def final_report(self, full_report=False):
+        print "wtf"
+        self.ch_delta=[]
+        self.ch_files=[]
+        self.ch_levels=[]
+        self.full_report=full_report
+        for delta, fname in self.changed_lines:
+            if full_report: print "changed", delta, fname
+            self.ch_delta.append(delta)
+            self.ch_files.append(fname)
+        for delta, fname in self.new_lines:
+            if full_report: print "new", delta, fname
+            self.ch_delta.append(delta)
+            self.ch_files.append(fname)
+        for delta, fname in self.removed_lines:
+            if full_report: print "gone", delta, fname
+            self.ch_delta.append(delta)
+            self.ch_files.append(fname)
+        for fname in self.ch_files:
+            self.ch_levels.append(fname.count("/"))
+        if '/' in self.ch_files:
+            self.ch_levels[ self.ch_files.index('/')] = 0
+
+        self.all_levels=nar([fname.count('/') for fname in self.files])
+        self.all_levels[self.files.index('/')] = 0
+
+        #all_levels[ all_files.index('/')] = 0
+        self.ch_delta= nar(self.ch_delta)
+        self.ch_files= nar(self.ch_files)
+        self.ch_levels= nar(self.ch_levels)
 
 def report(d1, d2, n1, n2, message):
     #longest = max([d1.longest_line,d2.longest_line])
@@ -120,13 +156,12 @@ def report(d1, d2, n1, n2, message):
     if np.abs(delta) > 0:
         print template%(s1,message,s2)
 
-def delta(d1, d2, nchecks=None):
+def delta(d1, d2, nchecks=None, verb=0):
     """changes the *delta* value in d2 to reflect changes
     Files the disappear from d1 are maybe not obvious how to deal with..."""
     if nchecks is None:
         nchecks = max(len(d1.lines),len(d2.lines))
     n1=0; n2=0
-    verb = -1
     #longest = max([d1.longest_line,d2.longest_line])
     #template = "%"+str(longest)+"s |%s|  %"+str(longest)+"s"
     for n_counter in range(nchecks):  #n1 + n2 < len(d1.files)+ len(d2.files):
@@ -207,7 +242,7 @@ duud_list = []
 for filename in args:
     print "=======", filename
     the_duud= duud(filename)
-    the_duud.ingest(slice(10)) #slice(3)) 
+    the_duud.ingest( slice(options.slice)) #slice(3)) 
     #print the_duud.sizes_bytes
     duud_list.append(the_duud)
 
@@ -215,34 +250,12 @@ for n_duud, the_duud in enumerate(duud_list):
     if n_duud == 0:
         last_duud = the_duud
     else:
-        delta(last_duud, the_duud)#, nchecks=4)
+        delta(last_duud, the_duud, verb = options.verb)#, nchecks=4)
         last_duud=the_duud
-d1 = duud_list[0]
-d2 = duud_list[1]
+        the_duud.final_report()
 
-all_delta=[]
-all_files=[]
-all_levels=[]
-full_report=False
-for delta, fname in d2.changed_lines:
-    if full_report: print "changed", delta, fname
-    all_delta.append(delta)
-    all_files.append(fname)
-for delta, fname in d2.new_lines:
-    if full_report: print "new", delta, fname
-    all_delta.append(delta)
-    all_files.append(fname)
-for delta, fname in d2.removed_lines:
-    if full_report: print "gone", delta, fname
-    all_delta.append(delta)
-    all_files.append(fname)
-for fname in all_files:
-    all_levels.append(fname.count("/"))
+if 1:
+    d1 = duud_list[0]
+    d2 = duud_list[1]
 
-#all_levels[ all_files.index('/')] = 0
-print d1.lines[-1]
-print d2.lines[-1]
-all_delta= nar(all_delta)
-all_files= nar(all_files)
-all_levels= nar(all_levels)
 #end
